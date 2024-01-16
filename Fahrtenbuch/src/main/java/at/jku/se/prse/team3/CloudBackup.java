2bb10852a85bf690371b048bf212d64abc040768
agmilan <147155157+agmilan@users.noreply.github.com>
2024-01-16 02:28:19 +0100

    1- Change to the Exceptions to make it fine for pmd code check.

--- Fahrtenbuch/src/main/java/at/jku/se/prse/team3/CloudBackup.java
+++ Fahrtenbuch/src/main/java/at/jku/se/prse/team3/CloudBackup.java
@@ -9,6 +9,7 @@ import com.dropbox.core.v2.files.WriteMode;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serial;
 
 public class CloudBackup {
 
@@ -17,12 +18,24 @@ public class CloudBackup {
     //Custom exception for Dropbox upload errors.
 
     public static class CloudBackupException extends RuntimeException {
+        /**
+         * Constructs a new CloudBackupException with the specified detail message and cause.
+         *
+         * @param message The detail message (which is saved for later retrieval by the getMessage() method).
+         * @param cause   The cause (which is saved for later retrieval by the getCause() method).
+         */
         public CloudBackupException(String message, Throwable cause) {
             super(message, cause);
         }
     }
 
     public static class DropboxUploadException extends RuntimeException {
+        /**
+         * Constructs a new DropboxUploadException with the specified detail message and cause.
+         *
+         * @param message The detail message (which is saved for later retrieval by the getMessage() method).
+         * @param cause   The cause (which is saved for later retrieval by the getCause() method).
+         */
         public DropboxUploadException(String message, Throwable cause) {
             super(message, cause);
         }
@@ -31,6 +44,12 @@ public class CloudBackup {
     //Custom exception for file upload errors.
 
     public static class FileUploadException extends DbxException {
+        /**
+         * Constructs a new FileUploadException with the specified detail message and cause.
+         *
+         * @param message The detail message (which is saved for later retrieval by the getMessage() method).
+         * @param cause   The cause (which is saved for later retrieval by the getCause() method).
+         */
         public FileUploadException(String message, Throwable cause) {
             super(message, cause);
         }
@@ -44,7 +63,7 @@ public class CloudBackup {
      * @param accessToken Access Token um Transaktion zu authorisieren, jede 4 Stunden wird neue gebraucht.
      */
     public static void uploadDB(String localPath, String cloudPath, String accessToken) throws FileUploadException {
-        try (InputStream in = new FileInputStream(localPath)) {
+        try (InputStream input = new FileInputStream(localPath)) {
             DbxRequestConfig config = DbxRequestConfig.newBuilder(CLIENT_IDENTIFIER).build();
             DbxClientV2 client = new DbxClientV2(config, accessToken);
 
@@ -54,7 +73,7 @@ public class CloudBackup {
 
             FileMetadata metadata = client.files().uploadBuilder(cloudPath)
                     .withMode(WriteMode.OVERWRITE)
-                    .uploadAndFinish(in);
+                    .uploadAndFinish(input);
 
         } catch (IOException | UploadErrorException e) {
             throw new DropboxUploadException("Error uploading to Dropbox",e);
